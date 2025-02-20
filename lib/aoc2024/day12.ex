e22157f5753e0e7bc0cb9d6646c04b20b6aaf753
Hampus Backman <hampus@safetyamp.com>
2025-02-20 12:32:41 -0500

    day 12 part two example works, but not input

--- lib/aoc2024/day12.ex
+++ lib/aoc2024/day12.ex
@@ -68,25 +68,92 @@ defmodule Aoc2024.Day12 do
       |> Enum.sum()
   end
 
-  defp sides(map, nodes, chr) do
-    set = MapSet.new(nodes)
+  defp sides(map, nodes) do
+    scan_sides(map, nodes)
+  end
 
-    IO.puts "------------------ #{chr} ------------------"
-    Matrix.map(map, fn ch, {x, y} ->
-      if Enum.member?(nodes, {x, y}) do
-        ch
+  defp scan_sides(map, region) do
+    start = region
+      |> Enum.sort_by(fn {x, _} -> x end)
+      |> List.first()
+
+    scan_sides(map, region, [], start, %{
+      dir: :left,
+      pos: start,
+      turns: 0,
+      turn: nil,
+    })
+  end
+
+  # A single cell always has 4 sides.
+  defp scan_sides(_map, [_], _contour, _start, _info), do: 4
+
+  defp scan_sides(map, region, contour, start, info) do
+    %{
+      dir:   dir,
+      pos:   pos,
+      turns: turns,
+      turn:  turn,
+    } = info
+
+    {cx, cy} = pos
+
+    {nx, ny, nd} = case dir do
+      :left  -> {cx-1, cy, :up}
+      :up    -> {cx, cy-1, :right}
+      :right -> {cx+1, cy, :down}
+      :down  -> {cx, cy+1, :left}
+    end
+
+    #inspect_area(map, region, "#", [pos | contour])
+
+    #:timer.sleep(100)
+
+    if Enum.member?(region, {nx, ny}) do
+      nd = case dir do
+        :down  -> :up
+        :up    -> :down
+        :right -> :left
+        :left  -> :right
+      end
+
+      turns = if dir != turn,
+        do: turns + 1,
+        else: turns
+
+      # found its way back to the start
+      if {nx, ny} == start and nd == :left and not Enum.empty?(contour) do
+        turns
       else
-        "."
+        scan_sides(map, region, [{nx, ny} | contour], start, %{
+          dir: nd,
+          pos: {nx, ny},
+          turns: turns,
+          turn: dir,
+        })
       end
-    end) |> Matrix.inspect()
+    else
+      scan_sides(map, region, contour, start, %{
+        dir: nd,
+        pos: {nx, ny},
+        turns: turns,
+        turn: turn,
+      })
+    end
+  end
 
+  defp inspect_area(map, nodes, chr, extra \\ []) do
+    IO.puts "------------------ #{chr} ------------------"
+    map
+      |> Matrix.map(fn ch, {x, y} ->
+          cond do
+            Enum.member?(extra, {x, y}) -> "#"
+            Enum.member?(nodes, {x, y}) -> ch
+            true -> "."
+          end
+        end)
+      |> Matrix.inspect()
     IO.puts ""
-
-    Enum.reduce(nodes, 0, fn {x, y}, s ->
-      s + case edges({x, y}, set) do
-        v -> 0
-      end
-    end)
   end
 
   # M:  5 *  6 = 30
@@ -106,12 +173,19 @@ defmodule Aoc2024.Day12 do
     input
       |> Matrix.from()
       |> solve()
-      |> Enum.map(fn {seed, nodes} -> {seed, area(nodes), sides(map, nodes, seed)} end)
-      |> Enum.map(fn {seed, a, b} -> {seed, a, b} end)
+
+      #|> Enum.filter(fn {seed, _} -> seed == "C" end)
+      #|> Enum.reverse()
+      #|> Enum.take(1)
+
+      |> Enum.map(fn {seed, nodes} -> {seed, area(nodes), sides(map, nodes)} end)
+
+      #|> Enum.map(fn {_, a, b} -> a * b end)
+      #|> Enum.sum()
+
       |> Enum.each(fn {seed, a, b} ->
           aa = String.pad_leading("#{a}", 2, " ")
           bb = String.pad_leading("#{b}", 2, " ")
-
           IO.puts "#{seed}:   #{aa} * #{bb} = #{a*b}"
         end)
   end
